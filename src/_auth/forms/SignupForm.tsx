274b3a457c790ce479e4419e0d97247c05223fec
Brandon Newton <114821633+BengalPirate@users.noreply.github.com>
2024-06-30 13:08:52 -0500

    welcome and logo

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -37,7 +37,19 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/alt-logo-small.png" alt="logo"/>
+
+        <img src="/assets/images/alt-logo-small.png" alt="logo" style={{ width: '20%', height: '20%', marginBottom: '30px' }}/>
+
+        {/* Welcome Video */}
+        <video
+          src="/assets/videos/tech_welcome.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-auto object-cover bg-no-repeat mb-4"
+          style={{ marginBottom: '0px' }}
+        ></video>
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">To use Synapse, please enter your details</p>
