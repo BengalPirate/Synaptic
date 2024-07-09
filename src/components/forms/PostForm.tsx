4d9f3e6f6d52ffbff9942f52a4f5c7ce636729d3
Brandon Newton <114821633+BengalPirate@users.noreply.github.com>
2024-07-08 23:20:30 -0500

    post

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -1,37 +1,25 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
 
-import * as z from "zod";
-//import { Models } from "appwrite";
-import { useForm } from "react-hook-form";
-//import { useNavigate } from "react-router-dom";
-import { zodResolver } from "@hookform/resolvers/zod";
-
+import { Button } from "@/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-  Button,
-  Input,
-  Textarea,
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "@/components/ui/form";
-import { button } from "@/components/ui/button";
-//import { PostValidation } from "@/lib/validation";
-//import { useToast } from "@/components/ui/use-toast";
-//import { useUserContext } from "@/context/AuthContext";
-import { 
-    FileUploader, 
-    //Loader 
-} from "@/components/shared/FileUploader";
-//import { useCreatePost, useUpdatePost } from "@/lib/react-query/queries";
-
+import { Input } from "@/components/ui/input"
 
 const formSchema = z.object({
     username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
+        message: "Username must be at least 2 character.",
     }),
 })
+
 
 const PostForm = () => {
     // 1. Define your form.
@@ -48,83 +36,29 @@ const PostForm = () => {
         // This will be type-safe and validated.
         console.log(values)
     }
-
     return (
-        <form {...form}>
+        <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col gap-9 w-full max-w-5xl">
+            className="space-y-8">
                 <FormField
                     control={form.control}
-                    name="caption"
+                    name="username"
                     render={({ field }) => (
                         <FormItem>
-                            <FormLabel className="shad-form_label">Caption</FormLabel>
+                            <FormLabel>Username</FormLabel>
                             <FormControl>
-                                <Textarea className="shad-textarea custom-scrollbar" {...field}/>
+                                <Input placeholder="shadcn" {...field}/>
                             </FormControl>
-                            <FormMessage className="shad-form_message"/>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
                         </FormItem>
                     )}
                 />
-                <FormField
-                    control={form.control}
-                    name="file"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="shad-form_label">Add Photos</FormLabel>
-                            <FormControl>
-                                <FileUploader/>
-                            </FormControl>
-                            <FormMessage className="shad-form_message"/>
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="location"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="shad-form_label">Add Location</FormLabel>
-                            <FormControl>
-                                <Input type="text" className="shad-input"/>
-                            </FormControl>
-                            <FormMessage className="shad-form_message"/>
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="tags"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="shad-form_label">Add Tags (separated by comma " , ")</FormLabel>
-                            <FormControl>
-                                <Input 
-                                    type="text" 
-                                    className="shad-input"
-                                    placeholder="Art, Expression, Learn"
-                                />
-                            </FormControl>
-                            <FormMessage className="shad-form_message"/>
-                        </FormItem>
-                    )}
-                />
-                <div className="flex gap-4 items-center justify-end">
-                    <Button 
-                        type="button" 
-                        className="shad-button_dark_4"
-                    >
-                        Cancel
-                    </Button>
-                    <Button 
-                        type="submit"
-                        className="shad-button_primary whitespace-nowrap"
-                    >
-                        Submit
-                    </Button>
-                </div>
+                <Button type="submit">Submit</Button>
             </form>
-        </form>
+        </Form>
     )
 }
 
