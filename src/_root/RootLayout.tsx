44f7e63bfc5a3ec9167a73a2443ccd5d001b457d
Brandon Newton <114821633+BengalPirate@users.noreply.github.com>
2024-07-14 15:39:55 -0500

    close to mvp

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -3,11 +3,16 @@ import LeftSidebar from '@/components/shared/LeftSidebar'
 import Topbar from '@/components/shared/Topbar'
 
 import { Outlet } from 'react-router-dom'
+import { useGetCurrentUser } from '@/lib/react-query/queriesAndMutations';
+import LifestreamEffect from '../../styles/LifestreamEffect';
 
 const RootLayout = () => {
+  const { data: currentUser } = useGetCurrentUser();
+
   return (
-    <div className="w-full md:flex">
-      <Topbar/>
+    <div className="w-full md:flex relative">
+      {currentUser && <LifestreamEffect />} {/* Add the lifestream effect background for authenticated users */}
+      <Topbar />
       <LeftSidebar />
 
       <section className="flex flex-1 h-full">
