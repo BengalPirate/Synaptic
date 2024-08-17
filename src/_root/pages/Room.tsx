9c6c182cfb20c73938c01ebff3e0826e4a237cc1
devinscott25 <devintscott25@gmail.com>
2024-08-17 00:19:57 -0500

    Correct styling is now applied to their respective message.  Got rid of userId variable to user useUserContext variable.

--- src/_root/pages/Room.tsx
+++ src/_root/pages/Room.tsx
@@ -25,35 +25,51 @@ async function getMessagesDocumentById(id: string) {
 }
 
 async function getUserDocumentById(id: string) {
-    try {
-        const document = await databases.getDocument(appwriteConfig.databaseId, appwriteConfig.userCollectionId, id);
-        return document;
-    } catch (error) {
-        console.error(`ERROR FETCHING USER DOCUMENT ${id}: `, error);
-        return null; // or handle the error as needed
-    }
+  try {
+    const document = await databases.getDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      id
+    );
+    return document;
+  } catch (error) {
+    console.error(`ERROR FETCHING USER DOCUMENT ${id}: `, error);
+    return null; // or handle the error as needed
+  }
 }
 
 async function getRoomDocumentById(id: string) {
-    try {
-        const document = await databases.getDocument(appwriteConfig.databaseId, appwriteConfig.userCollectionId, id);
-        return document;
-    } catch (error) {
-        console.error(`ERROR FETCHING USER DOCUMENT ${id}: `, error);
-        return null; // or handle the error as needed
-    }
+  try {
+    const document = await databases.getDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      id
+    );
+    return document;
+  } catch (error) {
+    console.error(`ERROR FETCHING USER DOCUMENT ${id}: `, error);
+    return null; // or handle the error as needed
+  }
+}
+
+function formatTime(dateString: string): string {
+  const date = new Date(dateString);
+  const options: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
   };
+  return date.toLocaleTimeString("en-US", options);
+}
 
 const Room = () => {
   const { id } = useParams();
-  const { user } = useUserContext()
+  const { user } = useUserContext();
   const [messagesIds, setMessagesIds] = useState([]);
   const [messages, setMessages] = useState([]);
   const [room, setRoom] = useState({});
   const [messageBody, setMessageBody] = useState("");
-  const [userId, setUserId] = useState("");
-  const [otherUsernames, setOtherUsernames] = useState([]);	
-
+  const [otherUsernames, setOtherUsernames] = useState([]);
 
   // console.log(id);
 
@@ -68,20 +84,6 @@ const Room = () => {
     };
     fetchRoom();
   }, []);
-  
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const user = await account.get();
-        setUserId(user.$id);
-      } catch (error) {
-        console.error("ERROR FETCHING USER: ", error);
-      }
-    };
-    fetchUser();
-  }, []);
-
-  console.log(userId);
 
   useEffect(() => {
     const fetchRoomMessages = async () => {
@@ -93,128 +95,136 @@ const Room = () => {
     };
     fetchRoomMessages();
   }, [messagesIds]);
-  
+
   useEffect(() => {
-		const fetchOtherUsernames = async () => {
-			console.log("SETTING OTHER USERS...")
-			try {
-				await setOtherUsers()
-			} catch (error) {
-				console.error("ERROR FETCHING ROOM: ", error)
-			}
-		}
-		fetchOtherUsernames()
-	}, [room])
+    const fetchOtherUsernames = async () => {
+      console.log("SETTING OTHER USERS...");
+      try {
+        await setOtherUsers();
+      } catch (error) {
+        console.error("ERROR FETCHING ROOM: ", error);
+      }
+    };
+    fetchOtherUsernames();
+  }, [room]);
 
   const handleSubmit = async (e) => {
-		e.preventDefault()
+    e.preventDefault();
 
-		const otherUserIds = room!.user_ids.filter(id => id !== user.id)
-		const newMessageId = ID.unique()
+    const otherUserIds = room!.user_ids.filter((id) => id !== user.id);
+    const newMessageId = ID.unique();
 
-		console.log("OTHER USERNAMES FOR MESSAGE PAYLOAD: ", otherUsernames)
-		console.log("MESSAGE BODY: ", messageBody)
-		let messagePayload = {
-			sender_username: user.username,
-			sender_id: user.id,
-			body: messageBody,
-			receiver_usernames: otherUsernames,
-			receiver_ids: otherUserIds,
-			room_id: room.$id
-		}
-		
-		let createMessageResponse = await databases.createDocument(
-							appwriteConfig.databaseId, 
-							appwriteConfig.messagesCollectionId,
-							newMessageId,
-							messagePayload
-		)
+    console.log("OTHER USERNAMES FOR MESSAGE PAYLOAD: ", otherUsernames);
+    console.log("MESSAGE BODY: ", messageBody);
+    let messagePayload = {
+      sender_username: user.username,
+      sender_id: user.id,
+      body: messageBody,
+      receiver_usernames: otherUsernames,
+      receiver_ids: otherUserIds,
+      room_id: room.$id,
+    };
 
-		await updateRoomMessages(createMessageResponse)
+    let createMessageResponse = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.messagesCollectionId,
+      newMessageId,
+      messagePayload
+    );
 
-		console.log("MESSAGE CREATE RESPONSE: ", createMessageResponse)
-		
-		setMessages([...messages, createMessageResponse])
+    await updateRoomMessages(createMessageResponse);
 
-		console.log("UPDATED MESSAGES: ", messages)
+    console.log("MESSAGE CREATE RESPONSE: ", createMessageResponse);
 
-		setMessageBody('');
+    setMessages([...messages, createMessageResponse]);
 
-		let roomPayload = {
-			room_name: room.room_name,
-			user_ids: room.user_ids,
-			messages: [...room.messages, createMessageResponse.$id]
-		}
+    console.log("UPDATED MESSAGES: ", messages);
 
-		console.log("ROOM PAYLOAD: ", roomPayload)
+    setMessageBody("");
 
-		try {
-			const roomUpdateResponse = await databases.updateDocument(
-				appwriteConfig.databaseId,
-				appwriteConfig.roomsCollectionId,
-				room!.$id,
-				roomPayload
-			)
-			console.log("ROOM UPDATE RESPONSE: ", roomUpdateResponse)
-		} catch (error) {
-			console.log("ERROR UPDATING ROOM MESSAGES: ", error)
-		}
+    let roomPayload = {
+      room_name: room.room_name,
+      user_ids: room.user_ids,
+      messages: [...room.messages, createMessageResponse.$id],
+    };
 
-		
-	}
+    console.log("ROOM PAYLOAD: ", roomPayload);
+
+    try {
+      const roomUpdateResponse = await databases.updateDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.roomsCollectionId,
+        room!.$id,
+        roomPayload
+      );
+      console.log("ROOM UPDATE RESPONSE: ", roomUpdateResponse);
+    } catch (error) {
+      console.log("ERROR UPDATING ROOM MESSAGES: ", error);
+    }
+  };
   const getRoomMessagesIds = async () => {
-		console.log("ROOM ID: ", id)
-		if (id){
-			try{
-				const userRoom = await databases.getDocument(
-					appwriteConfig.databaseId,
-					appwriteConfig.roomsCollectionId,
-					id
-				)
-				console.log("ROOM: ", userRoom)
+    console.log("ROOM ID: ", id);
+    if (id) {
+      try {
+        const userRoom = await databases.getDocument(
+          appwriteConfig.databaseId,
+          appwriteConfig.roomsCollectionId,
+          id
+        );
+        console.log("ROOM: ", userRoom);
 
-				setMessagesIds(userRoom.messages);
-				setRoom(userRoom);
-			} catch (error) {
-				console.log("ERROR RECEIVING ROOM: ", error)
-			}
-		}
-	}
+        setMessagesIds(userRoom.messages);
+        setRoom(userRoom);
+      } catch (error) {
+        console.log("ERROR RECEIVING ROOM: ", error);
+      }
+    }
+  };
 
   const getMessages = async () => {
-		try {
-			const roomMessages = await Promise.all(messagesIds.map( async (id) => {
-				return await getMessagesDocumentById(id)
-			}))
-			console.log("ROOM MESSAGES: ", roomMessages)
-			setMessages(roomMessages)
-		} catch (error) {
-			console.log("ERROR FETCHING MESSAGES: "	, error)
-		}
-	}
-  
-  const setOtherUsers = async () => {
-		try {
-			const fetchedUsers = await Promise.all(room.user_ids.map( async (id) => {
-				return await getUserDocumentById(id)
-			}))
-			const otherFetchedUsers = fetchedUsers.filter(fetchedUser => fetchedUser.username !== user.username)
-			console.log("OTHER USERS: ", otherFetchedUsers)
-			const otherUsernamesToBeSet = otherFetchedUsers.map(fetchedUser => fetchedUser.username);
-			console.log("OTHER USERNAMES: ", otherUsernamesToBeSet)
-			setOtherUsernames(otherUsernamesToBeSet)
-		} catch (error) {
-			console.log("ERROR FETCHING OTHER USERS", error)
-		}
-	}
+    try {
+      const roomMessages = await Promise.all(
+        messagesIds.map(async (id) => {
+          return await getMessagesDocumentById(id);
+        })
+      );
+      console.log("ROOM MESSAGES: ", roomMessages);
+      setMessages(roomMessages);
+    } catch (error) {
+      console.log("ERROR FETCHING MESSAGES: ", error);
+    }
+  };
 
-	const updateRoomMessages = async (newMessage) => {
-		setRoom(prevRoom => ({
-			...prevRoom,
-			messages: [...prevRoom.messages, newMessage.$id]
-		}));
-	};
-  
+  const setOtherUsers = async () => {
+    try {
+      const fetchedUsers = await Promise.all(
+        room.user_ids.map(async (id) => {
+          return await getUserDocumentById(id);
+        })
+      );
+      const otherFetchedUsers = fetchedUsers.filter(
+        (fetchedUser) => fetchedUser.username !== user.username
+      );
+      console.log("OTHER USERS: ", otherFetchedUsers);
+      const otherUsernamesToBeSet = otherFetchedUsers.map(
+        (fetchedUser) => fetchedUser.username
+      );
+      console.log("OTHER USERNAMES: ", otherUsernamesToBeSet);
+      setOtherUsernames(otherUsernamesToBeSet);
+    } catch (error) {
+      console.log("ERROR FETCHING OTHER USERS", error);
+    }
+  };
+
+  const updateRoomMessages = async (newMessage) => {
+    setRoom((prevRoom) => ({
+      ...prevRoom,
+      messages: [...prevRoom.messages, newMessage.$id],
+    }));
+  };
+
+  console.log(`User context is located here: ${user.id}`);
+
   return (
     <main className="container">
       <div className="room--format">
@@ -236,14 +246,14 @@ const Room = () => {
                 <div
                   key={message.$id}
                   className={`message--wrapper ${
-                    message.sender_id === userId
-                      ? "other-message"
-                      : "my-message"
+                    message.sender_id === user.id
+                      ? "my-message"
+                      : "other-message"
                   }`}
                 >
                   <div className="message--header">
                     <small className="message-timestamp">
-                      {message.$createdAt}
+                      {formatTime(message.$createdAt)}
                     </small>
                   </div>
 
@@ -281,4 +291,3 @@ const Room = () => {
   );
 };
 export default Room;
-
